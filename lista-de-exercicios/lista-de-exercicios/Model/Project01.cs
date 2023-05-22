735e91bb18c4c6cdaef8cd7ae4379b0c9463d699
Modena <felipe.kroitor@gmail.com>
2023-05-22 16:24:13 -0300

    deploy: project 1 finished

--- lista-de-exercicios/lista-de-exercicios/Model/Project01.cs
+++ lista-de-exercicios/lista-de-exercicios/Model/Project01.cs
@@ -8,29 +8,91 @@ namespace lista_de_exercicios.Model
 {
     internal class Project01 : Interface.IProject1
     {
-        public void Divide()
-        {
-            throw new NotImplementedException();
-        }
-
         public int Menu()
         {
-            throw new NotImplementedException();
+            int option;
+
+            do
+            {
+                printMenuOptions();
+                option = int.Parse(Console.ReadLine());
+                Console.WriteLine(option == 1);
+
+                switch (option)
+                {
+                    case 1:
+                        Soma();
+                        break;
+                    case 2:
+                        Subtrai();
+                        break;
+                    case 3:
+                        Multiplica();
+                        break;
+                    case 4:
+                        Divide();
+                        break;
+                    default:
+                        break;
+                }
+            } while (option != 0);
+
+            return option;
         }
 
-        public void Multiplica()
+        private static void printMenuOptions()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Escolha a operação que deseja realizar: ");
+
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
         }
 
         public void Soma()
         {
-            throw new NotImplementedException();
+           int num1, num2, result;
+            Console.WriteLine("Digite o primeiro número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+            result = num1 + num2;
+            Console.WriteLine("O resultado da soma é: " + result);
         }
 
         public void Subtrai()
         {
-            throw new NotImplementedException();
+            int num1, num2, result;
+            Console.WriteLine("Digite o primeiro número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+            result = num1 - num2;
+            Console.WriteLine("O resultado da soma é: " + result);
+
+        }
+        public void Divide()
+        {
+            int num1, num2, result;
+            Console.WriteLine("Digite o primeiro número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+            result = num1 / num2;
+            Console.WriteLine("O resultado da soma é: " + result);
+        }
+
+
+        public void Multiplica()
+        {
+            int num1, num2, result;
+            Console.WriteLine("Digite o primeiro número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+            result = num1 * num2;
+            Console.WriteLine("O resultado da soma é: " + result);
         }
     }
 }
--- lista-de-exercicios/lista-de-exercicios/Program.cs
+++ lista-de-exercicios/lista-de-exercicios/Program.cs
@@ -16,39 +16,71 @@ namespace lista_de_exercicios
         private static readonly List3 list3 = new List3();
         static void Main(string[] args)
         {
-            int option;
-            List<String> listsSelected = new List<String>();
 
+            Console.WriteLine("Quer ver projeto ou lista de exercícios?");
+            Console.WriteLine("1 - Projeto");
+            Console.WriteLine("2 - Lista de exercícios");
+
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    chooseProject();
+                    break;
+                case 2:
+                    chooseList();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void chooseProject()
+        {
+
+            Console.WriteLine("Escolha o número do projeto que deseja executar: ");
+            Console.WriteLine("1 - Projeto 1: Calculadora");
+
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    Project01 project01 = new Project01();
+                    project01.Menu();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void chooseList()
+        {
+            int option;
+            List<string> listsSelected = new List<string>();
             Console.WriteLine("Escolha o número da lista de exercícios que deseja executar: ");
             Console.WriteLine("1 - Lista de exercícios 1");
             Console.WriteLine("2 - Lista de exercícios 2");
             Console.WriteLine("3 - Lista de exercícios 3");
-
-
             Console.WriteLine("Digite o número da lista de exercícios: ");
             option = int.Parse(Console.ReadLine());
             Console.WriteLine("\n\n ");
 
             listsSelected.Add(option.ToString());
 
-     
-                switch (option)
-                {
-
-                    case 1:
-                        list1.Menu();
-                        break;
-                    case 2:
-                        list2.Menu();
-                        break;
-                    case 3:
-                        list3.Menu();
-                        break;
-                    default:
-                        Console.WriteLine("Opção inválida!");
-                        listsSelected.RemoveAt(listsSelected.Count - 1);
-                        break;
-                }
+            switch (option)
+            {
+                case 1:
+                    list1.Menu();
+                    break;
+                case 2:
+                    list2.Menu();
+                    break;
+                case 3:
+                    list3.Menu();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    listsSelected.RemoveAt(listsSelected.Count - 1);
+                    break;
+            }
         }
     }
 }
