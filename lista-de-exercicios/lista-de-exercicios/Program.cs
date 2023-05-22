9ea2b4190f0633a7b93d233725ccd3376c45e3cf
Modena <felipe.kroitor@gmail.com>
2023-05-22 09:34:10 -0300

    update: refactor on Program

--- lista-de-exercicios/lista-de-exercicios/Program.cs
+++ lista-de-exercicios/lista-de-exercicios/Program.cs
@@ -1,4 +1,5 @@
 ﻿using lista_de_exercicios.Lists;
+using lista_de_exercicios.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,9 @@ namespace lista_de_exercicios
 {
     internal class Program
     {
-        private static List1 list1 = new List1();
-        private static List2 List2 = new List2();
+        private static readonly List1 list1 = new List1();
+        private static readonly List2 list2 = new List2();
+        private static readonly List3 list3 = new List3();
         static void Main(string[] args)
         {
             int option;
@@ -30,7 +32,10 @@ namespace lista_de_exercicios
                     list1.Menu();
                     break;
                 case 2:
-                    List2.Menu();
+                    list2.Menu();
+                    break;
+                case 3:
+                    list3.Menu();
                     break;
                 default:
                     Console.WriteLine("Opção inválida!");
